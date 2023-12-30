9d8a63278fdef91692076336a302aed586b6aaef
Dávid Kocsis <kocsis1david@windowslive.com>
2023-12-30 14:45:22 +0100

    kam keybindings

--- src/kbd/handler.rs
+++ src/kbd/handler.rs
@@ -79,6 +79,7 @@ impl KeyboardHandler {
 
     pub fn run_forever(&mut self) {
         let mut caps = false;
+        let mut kam = false;
 
         std::thread::sleep(std::time::Duration::from_secs(1));
 
@@ -101,60 +102,121 @@ impl KeyboardHandler {
                 caps = input.value != 0;
 
                 if input.value == 0 {
-                    for x in caps_keys.drain(..) {
-                        self.write(&input_event {
-                            time: input.time,
-                            type_: 1,
-                            code: x,
-                            value: 0,
-                        });
-                    }
+                    self.release_keys(&mut caps_keys, input.time);
                 }
 
                 continue;
             }
 
-            if caps && !other_keys.contains(&input.code) {
-                let key_to_press = match input.code {
-                    KEY_I => Some(KEY_UP),
-                    KEY_J => Some(KEY_LEFT),
-                    KEY_K => Some(KEY_DOWN),
-                    KEY_L => Some(KEY_RIGHT),
-                    KEY_U => Some(KEY_HOME),
-                    KEY_O => Some(KEY_END),
-                    KEY_P => Some(KEY_PAGEUP),
-                    KEY_SEMICOLON => Some(KEY_PAGEDOWN),
-                    KEY_A => Some(KEY_LEFTSHIFT),
-                    KEY_D => Some(KEY_LEFTCTRL),
-                    KEY_BACKSPACE => Some(KEY_DELETE),
-                    KEY_1 => Some(KEY_F1),
-                    KEY_2 => Some(KEY_F2),
-                    KEY_3 => Some(KEY_F3),
-                    KEY_4 => Some(KEY_F4),
-                    KEY_5 => Some(KEY_F5),
-                    KEY_6 => Some(KEY_F6),
-                    KEY_7 => Some(KEY_F7),
-                    KEY_8 => Some(KEY_F8),
-                    KEY_9 => Some(KEY_F9),
-                    KEY_0 => Some(KEY_F10),
-                    KEY_MINUS => Some(KEY_F11),
-                    KEY_EQUAL => Some(KEY_F12),
-                    _ => None,
+            if kam {
+                input.code = match input.code {
+                    KEY_W => KEY_UP,
+                    KEY_A => KEY_LEFT,
+                    KEY_S => KEY_DOWN,
+                    KEY_D => KEY_RIGHT,
+                    KEY_Q => KEY_MINUS,
+                    KEY_E => {
+                        input.code = KEY_LEFTSHIFT;
+                        add_or_remove_key(&mut other_keys, input.value, input.code);
+                        self.write(&input);
+                        KEY_EQUAL
+                    }
+                    KEY_Z => KEY_S,
+                    KEY_X => KEY_L,
+                    KEY_C => KEY_H,
+                    KEY_R => KEY_9,
+                    KEY_F => KEY_8,
+                    // caps + space => x
+                    x => x,
                 };
-                // println!("{:?} => {:?}", input.code, key_to_press);
 
-                if let Some(key_to_press) = key_to_press {
-                    add_or_remove_key(&mut caps_keys, input.value, key_to_press);
+                if caps && !other_keys.contains(&input.code) {
+                    let key_to_press = match input.code {
+                        KEY_SPACE => Some(KEY_X),
 
-                    input.code = key_to_press;
-                    self.write(&input);
-                    continue;
+                        KEY_COMPOSE => {
+                            if input.value == 0 {
+                                self.release_keys(&mut other_keys, input.time);
+                                self.release_keys(&mut caps_keys, input.time);
+                                kam = false;
+                            }
+                            continue;
+                        }
+                        _ => None,
+                    };
+
+                    if let Some(key_to_press) = key_to_press {
+                        add_or_remove_key(&mut caps_keys, input.value, key_to_press);
+
+                        input.code = key_to_press;
+                        self.write(&input);
+                        continue;
+                    }
+                }
+            } else {
+                if caps && !other_keys.contains(&input.code) {
+                    // dbg!(input.code);
+                    let key_to_press = match input.code {
+                        KEY_I => Some(KEY_UP),
+                        KEY_J => Some(KEY_LEFT),
+                        KEY_K => Some(KEY_DOWN),
+                        KEY_L => Some(KEY_RIGHT),
+                        KEY_U => Some(KEY_HOME),
+                        KEY_O => Some(KEY_END),
+                        KEY_P => Some(KEY_PAGEUP),
+                        KEY_SEMICOLON => Some(KEY_PAGEDOWN),
+                        KEY_A => Some(KEY_LEFTSHIFT),
+                        KEY_D => Some(KEY_LEFTCTRL),
+                        KEY_BACKSPACE => Some(KEY_DELETE),
+                        KEY_1 => Some(KEY_F1),
+                        KEY_2 => Some(KEY_F2),
+                        KEY_3 => Some(KEY_F3),
+                        KEY_4 => Some(KEY_F4),
+                        KEY_5 => Some(KEY_F5),
+                        KEY_6 => Some(KEY_F6),
+                        KEY_7 => Some(KEY_F7),
+                        KEY_8 => Some(KEY_F8),
+                        KEY_9 => Some(KEY_F9),
+                        KEY_0 => Some(KEY_F10),
+                        KEY_MINUS => Some(KEY_F11),
+                        KEY_EQUAL => Some(KEY_F12),
+
+                        KEY_COMPOSE => {
+                            if input.value == 0 {
+                                self.release_keys(&mut other_keys, input.time);
+                                self.release_keys(&mut caps_keys, input.time);
+                                kam = true;
+                            }
+                            continue;
+                        }
+                        _ => None,
+                    };
+                    // println!("{:?} => {:?}", input.code, key_to_press);
+
+                    if let Some(key_to_press) = key_to_press {
+                        add_or_remove_key(&mut caps_keys, input.value, key_to_press);
+
+                        input.code = key_to_press;
+                        self.write(&input);
+                        continue;
+                    }
                 }
             }
 
             // Pass-through
             add_or_remove_key(&mut other_keys, input.value, input.code);
             self.write(&input);
+        }
+    }
+
+    fn release_keys(&mut self, caps_keys: &mut Vec<u16>, time: libc::timeval) {
+        for x in caps_keys.drain(..) {
+            self.write(&input_event {
+                time,
+                type_: 1,
+                code: x,
+                value: 0,
+            });
         }
     }
 }
